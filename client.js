a812cc9b7ec186e763013258e1c8a4ac1859c6dc
369koushil <koushil463@gmail.com>
2025-04-24 15:31:05 +0530

    added a bsic 3 way handshaking like in tcp connection

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
 const{createConnection}=require("node:net")
+const { buffer } = require("node:stream/consumers")
 
-const client=createConnection({
-    host:'localhost',
-    port:'4000',
-}
+const client=createConnection({host:'localhost',port:'4000'},(()=>{
+    client.write(`<SOM>\nSYN\n<EOM>`)
+})
 )
 
 client.on('data',(data)=>{
-    console.log(`${data.toString()} received from server`)
+    const response=Buffer.from(data)
+    const msg=response.toString()
+    console.log(msg)
+    if (msg.toString().includes('<SOM>\nACK\n<EOM>')) {
+        client.write('<SOM>\nREADY\n<EOM>');
+    }
+
+    if(msg.toString().includes('<SOM>\nREADY\n<EOM>')){
+        console.log('handshake completed succesfully')
+        client.end()
+    }
+    
 })
 
 client.on('end',()=>{
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const{createServer}=require('node:net')
+const { buffer } = require('node:stream/consumers')
 
 const server=createServer((c)=>{
-    console.log("client connected")
-    c.on('end',c=>{
-        console.log(`client ${c} disconnected`)
+    console.log(`client ${c.remotePort} connected`)
+    c.on('end',()=>{
+        console.log(`client ${c.remotePort} disconnected`)
     })
-    c.write('hello from server')
+    
+    c.on('data',(data)=>{
+        const response=Buffer.from(data)
+        const msg=response.toString()
+        console.log(msg)
+        if(msg.includes("<SOM>\nSYN\n<EOM>")){
+           c.write(Buffer.from("<SOM>\nACK\n<EOM>"))
+        }
+        if(msg.includes('<SOM>\nREADY\n<EOM>')){
+            c.write(Buffer.from('<SOM>\nREADY\n<EOM>'))
+        }
+    })
 })
 
-server.listen(4000)
+server.listen(4000,()=>{
+    console.log('TCP server running on port 4000')
+})
